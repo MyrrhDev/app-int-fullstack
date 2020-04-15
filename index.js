2cf5cd8955379d8d82bfafa7f957bca46ae0691f
Mayra <mayra.pastor@est.fib.upc.edu>
2020-04-16 00:36:28 +0200

    extra credit 1 achieved

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
+
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+const strings = {
+    var1: {
+        title: 'This is variant one',
+        h1: 'This is OG variant one',
+        p: 'Let me tell you about this github repo:',
+        a: 'this project github repo',
+        url: 'https://github.com/MyrrhDev/app-int-fullstack',
+    }, 
+    var2: {
+        title: 'This is variant two',
+        h1: 'Variant two: the sequel',
+        p: 'Let me tell you about this git powered website:',
+        a: 'my website',
+        url: 'http://myrrhdev.github.io/',
+    },
+}
+
+class ElementHandler {
+  constructor(variant) {
+    this.variant = variant
+  }
+
+  element(element) {
+      console.debug(element.attributes);      
+      if(element.tagName == 'a') {
+          element.setInnerContent(this.variant[element.tagName])
+          element.setAttribute('href', this.variant['url'])
+      } else {
+          element.setInnerContent(this.variant[element.tagName])
+      }
+  }
+}
+
+
 async function handleRequest(request) {
     const url = "https://cfw-takehome.developers.workers.dev/api/variants";
     var re_urls = [ ]
+    var variant = [ ]
     
     return fetch(url, {
         method: 'GET',
@@ -23,17 +60,33 @@ async function handleRequest(request) {
             console.debug(re_urls);
             return response;
         })
-        .then(function(response) {
+        .then(async function(response) {
             if(Math.random() < 0.5) {
-                return fetch(re_urls[0]);
+                console.debug(strings['var1']);
+                var rewriter = new HTMLRewriter()
+                .on('title', new ElementHandler(strings['var1']))
+                .on('h1#title', new ElementHandler(strings['var1']))
+                .on('p#description', new ElementHandler(strings['var1']))
+                .on('a#url', new ElementHandler(strings['var1']))
+                .on('a', new ElementHandler(strings['var1']))
+                const res = await fetch(re_urls[0])
+                return rewriter.transform(res)
             } else {
-                return fetch(re_urls[1]);
+//                 console.debug(strings['var1']);
+                var rewriter = new HTMLRewriter()
+                .on('title', new ElementHandler(strings['var2']))
+                .on('h1#title', new ElementHandler(strings['var2']))
+                .on('p#description', new ElementHandler(strings['var2']))
+                .on('a#url', new ElementHandler(strings['var2']))
+                .on('a', new ElementHandler(strings['var2']))
+                const res = await fetch(re_urls[1])
+                return rewriter.transform(res)
             }
         })
-        .then(response => {
-            console.debug(response);
-            return new Response(response.body)            
-        })
+//         .then(response => {
+//             console.debug(response);
+//             return new Response(response.body)            
+//         })
         .catch(error => {
             console.error(error);
         });
