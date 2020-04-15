36c74fa7b2d2f67e2ee0eedb40f3545eab187e66
Mayra <mayra.pastor@est.fib.upc.edu>
2020-04-15 11:15:19 +0200

    json fetch urls

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
+
+
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+    const url = "https://cfw-takehome.developers.workers.dev/api/variants";
+
+    let method =	{ 
+        method: 'GET'
+    }
+    return fetch(url, method)
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                return response.text()
+                console.log(response)
+                throw new Error('Something went wrong on api server!');
+            }
+        })
+        .then(response => {
+            urls = response["variants"];
+            console.debug(response["variants"]);
+            return new Response(response)
+        }).catch(error => {
+            console.error(error);
+        });
 }
