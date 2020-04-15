53a312ca60e6ad3d89ad6d2442c3a79aa76e2b3f
Mayra <mayra.pastor@est.fib.upc.edu>
2020-04-15 19:14:08 +0200

    to Test: re2 & req 3

--- index.js
+++ index.js
@@ -2,30 +2,42 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-
+// // Math.random() < 0.5;
 
 
 async function handleRequest(request) {
     const url = "https://cfw-takehome.developers.workers.dev/api/variants";
 
-    let method =	{ 
-        method: 'GET'
-    }
-    return fetch(url, method)
-        .then(response => {
+    return fetch(url, {
+        method: 'get',
+        }).then(response => {
             if (response.status === 200) {
                 return response.json();
             } else {
                 return response.text()
                 console.log(response)
-                throw new Error('Something went wrong on api server!');
+                throw new Error('Something went wrong!');
             }
         })
         .then(response => {
             urls = response["variants"];
             console.debug(response["variants"]);
-            return new Response(response)
-        }).catch(error => {
+//             return new Response(response)
+            return response["variants"];
+            
+        })
+        .then(function(response) {
+            if(Math.random() < 0.5) {
+                return fetch(urls[0]);
+            } else {
+                return fetch(urls[1]);
+            }
+        })
+        .then(response => {
+            console.debug(response);
+            return new Response(response)            
+        })
+        .catch(error => {
             console.error(error);
         });
 }
